e4d8d4e2d4521b696aa00859c0d2be141cc827b1
sebastianr <sebastianr@serfe.com>
2025-09-22 20:01:32 -0300

    Estructura inicial del backend

--- src/main/java/com/ics/contactsapi/controller/ContactController.java
+++ src/main/java/com/ics/contactsapi/controller/ContactController.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/contacts")
 public class ContactController {
-
+// Generar operaciones CRUD. List Get Create Update Delete
 }
